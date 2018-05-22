a5cc5b2251e25cc835886271a8c1969d8557b91d
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-05-21 17:35:21 -0700

    add omdb call

--- liri.js
+++ liri.js
@@ -42,4 +42,22 @@ function playSongs(){
       });
 }
 
+//OMDB function
+function omdbRatings(){
+    var movieName = "Mr Nobody";
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    Request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Movie Title: " + JSON.parse(body).Title);
+            console.log("IMDB Rating: " + JSON.parse(body).Rated);
+            console.log("IMDB Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country produced: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
 
